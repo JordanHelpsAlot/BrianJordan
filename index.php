8ffcc7ee11f714f1f29aba20b66c0493fa29d429
jordanhelpsalot <jordanh1466@gmail.com>
2019-03-02 18:45:56 -0500

    updating readme

--- index.php
+++ index.php
@@ -16,26 +16,28 @@
   </head>
 
   <body>
-    <img src="abed_is_cool.jpg" style="width: 200px; margin-left: auto; margin-right: auto; display: inline;">
-    <p>Hey Abed. This is your website. It will obey you.</p>
-    <form action="index.php" method="POST">
-	    <label for="wish">Enter Your Wishes</label>
-	    <input name="wish" type="text" />
-	</form>
-	<div>
-		<?php
-			$wish = $_POST['wish'];
-			if(!empty($wish)){
-				echo "<div>";
-				if($wish=="spank me"){
-					echo "Your punishment will come when you least deserve it";
+  	<div style="width:100%;">
+	    <img src="abed_is_cool.jpg" style="width: 200px; margin-left: auto; margin-right: auto; display: inline;">
+	    <p>Hey Abed. This is your website. It will obey you.</p>
+	    <form action="index.php" method="POST">
+		    <label for="wish">Enter Your Wishes</label>
+		    <input name="wish" type="text" />
+		</form>
+		<div>
+			<?php
+				$wish = $_POST['wish'];
+				if(!empty($wish)){
+					echo "<div>";
+					if($wish=="spank me"){
+						echo "Your punishment will come when you least deserve it";
+					}
+					else{
+						echo "$wish: Granted";
+					}
+					echo "</div>";
 				}
-				else{
-					echo "$wish: Granted";
-				}
-				echo "</div>";
-			}
-		?>
+			?>
+		</div>
 	</div>
   </body>
 </html>
