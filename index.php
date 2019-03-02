792a10836698cb4516d4f9ffed0c6403be2130be
jordanhelpsalot <jordanh1466@gmail.com>
2019-03-02 11:00:32 -0500

    testing ssh connectivity

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <?php
+/*
 	$adjective = "<3";
 	echo "Abed is...";
 	for ($i = 1; $i<9; $i++){
 		echo "<div>".$i.": $adjective</div>";
 	}
 	echo "<div>For real.</div>";
+	*/
 ?>
 
 <!doctype html>
 <html>
   <head>
-    <title>My first webpage</title>
+    <title>Hey babe</title>
   </head>
 
   <body>
-    <img src="mypic.jpg">
-    <p>This is an example paragraph. Anything in the <strong>body</strong> tag will appear on the page, just like this <strong>p</strong> tag and its contents.</p>
-    <a href="myPHP.php">Click Me</a>
+    <img src="abed_is_cool.jpg">
+    <p>Hey Abed. This is your website. It will obey you.</p>
+    <label for="a">Enter Your Wishes</label>
+    <input name="a" type="text" />
+
   </body>
 </html>
