8e4f85bca021bb0bac39736a80617877db8f2e90
root <jordanh1466@gmail.com>
2019-03-07 01:09:31 +0000

    test

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<?php
-/*
-	$adjective = "<3";
-	echo "Abed is...";
-	for ($i = 1; $i<9; $i++){
-		echo "<div>".$i.": $adjective</div>";
-	}
-	echo "<div>For real.</div>";
-	*/
-?>
-
 <!doctype html>
 <html>
   <head>
@@ -16,28 +5,27 @@
   </head>
 
   <body>
-  	<div style="width:100%;">
-	    <img src="abed_is_cool.jpg" style="width: 200px; margin-left: auto; margin-right: auto; display: inline;">
-	    <p>Hey Abed. This is your website. It will obey you.</p>
-	    <form action="index.php" method="POST">
-		    <label for="wish">Enter Your Wishes</label>
-		    <input name="wish" type="text" />
-		</form>
-		<div>
-			<?php
-				$wish = $_POST['wish'];
-				if(!empty($wish)){
-					echo "<div>";
-					if($wish=="spank me"){
-						echo "Your punishment will come when you least deserve it";
-					}
-					else{
-						echo "$wish: Granted";
-					}
-					echo "</div>";
+	<h1> Abed's Wish Generator </h1>
+    <img src="abed_is_cool.jpg" style="width: 200px; margin-left: auto; margin-right: auto; display: inline;">
+    <p>Hey Abed. This is your website. It will obey you. <a href="/test.php"> Link Somewhere else</a></p>
+    <form action="index.php" method="POST">
+	    <label for="wish">Enter Your Wishes</label>
+	    <input name="wish" type="text" />
+	</form>
+	<div>
+		<?php
+			$wish = $_POST['wish'];
+			if(!empty($wish)){
+				echo "<div>";
+				if($wish=="spank me"){
+					echo "Your punishment will come when you least deserve it";
 				}
-			?>
-		</div>
+				else{
+					echo "$wish: Granted";
+				}
+				echo "</div>";
+			}
+		?>
 	</div>
   </body>
 </html>
