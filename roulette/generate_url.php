b62ca9371f38a4cca1dcdbd0e2401ca3b7862f69
jordanhelpsalot <jordanh1466@gmail.com>
2019-04-27 10:03:28 -0400

    adding url generator

--- roulette/generate_url.php
+++ roulette/generate_url.php
@@ -1,12 +1,12 @@
 <?php
-$test = $_POST['choose_show'];
-echo $test;
 
 $base_url = "https://www.netflix.com/watch/";
 $show_array[0]["id"] = "771476";
 $show_array[0]["length"] = 5520;
+$show_array[1]["id"] = "555221";
+$show_array[1]["length"] = 6350;
 
-$curr_show_index = 0;
+$curr_show_index = $_POST['choose_show'];
 $random_t = rand(1, $show_array[$curr_show_index]["length"]);
 
 $random_url = $base_url.$show_array[$curr_show_index]["id"].'?t='.$random_t;
