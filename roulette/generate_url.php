dacb249dc4c20e0bd80e8c55200e1780d2348597
jordanhelpsalot <jordanh1466@gmail.com>
2019-04-27 08:25:23 -0400

    adding url generator

--- roulette/generate_url.php
+++ roulette/generate_url.php
@@ -1,8 +1,13 @@
 <?php
+$base_url = "https://www.netflix.com/watch/";
+$show_array[0]["id"] = "771476";
+$show_array[0]["length"] = 5520;
 
-echo "here";
-$show_id = 4;
-echo $show_id;
+$curr_show_index = 0;
+$random_t = rand(1, $show_array[$curr_show_index]["length"]);
+
+echo $random_t;
+
 
 
 ?>
